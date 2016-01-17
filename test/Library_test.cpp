ba69f8e38861b65394431e89d0d81c808ef5e704
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2016-01-17 03:06:57 +0100

    Library

--- test/Library_test.cpp
+++ test/Library_test.cpp
@@ -1,9 +1,13 @@
 #include <gtest/gtest.h>
+#include <Library.hpp>
 #include <Bookmark.hpp>
 #include <memory>
 
 struct LibraryTest : public testing::Test {
 	Library library;
+	void SetUp() {
+		library.add(BookmarkPtr{new Bookmark{"a", "b", "c", Tags{}}});
+	}
 };
 
 TEST_F(LibraryTest, CanAddBookmark) {
