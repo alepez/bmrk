c2920a59cfb765e4370d5372ae4c805a17e80433
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2016-01-17 01:34:43 +0100

    Bookmark

---
+++ src/Bookmark.hpp
@@ -0,0 +1,14 @@
+#ifndef BOOKMARK_HPP_4NOOTDNA
+#define BOOKMARK_HPP_4NOOTDNA
+
+#include <string>
+#include <vector>
+
+class Bookmark {
+	const std::string url;
+	const std::string title;
+	const std::string notes;
+	const std::vector<std::string> tags;
+};
+
+#endif /* end of include guard: BOOKMARK_HPP_4NOOTDNA */
