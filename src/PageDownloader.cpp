b3d2442a024701fcd8af80bb0be0cf44ec0af549
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2016-01-17 01:06:15 +0100

    code clean

--- src/PageDownloader.cpp
+++ src/PageDownloader.cpp
@@ -12,19 +12,19 @@ template <typename Callback> void downloadWithCurl(const std::string& url, Callb
 	auto curl = curl_easy_init();
 	curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
 	curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0);
-	curl_easy_setopt(curl, CURLOPT_TIMEOUT, 15);
+	curl_easy_setopt(curl, CURLOPT_TIMEOUT, 5);
 	curl_easy_setopt(curl, CURLOPT_WRITEDATA, data);
 	curl_easy_setopt(curl, CURLOPT_ACCEPT_ENCODING, "gzip");
-
 	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, static_cast<CurlWriteFunction>(fn));
 	if (CURLE_OK != curl_easy_perform(curl)) {
 		curl_easy_cleanup(curl);
-		throw std::runtime_error("Cannot perform download " + url);
+		throw std::runtime_error("Cannot download " + url);
 	}
 	curl_easy_cleanup(curl);
 }
 
-template <typename T> void downloadToStreamWithCurl(const std::string& url, T* stream) {
+template <typename T>
+void downloadToStreamWithCurl(const std::string& url, T* stream) {
 	downloadWithCurl(url, [](void* ptr, size_t size, size_t nmemb, void* data) {
 		auto stream = reinterpret_cast<T*>(data);
 		stream->write(reinterpret_cast<char*>(ptr), size * nmemb);
@@ -35,10 +35,9 @@ template <typename T> void downloadToStreamWithCurl(const std::string& url, T* s
 } // end namespace
 
 std::future<std::string> PageDownloader::load(const std::string& url) {
-	auto handle = std::async(std::launch::async, [url]() {
+	return std::async(std::launch::async, [url]() {
 		std::stringstream stream;
 		downloadToStreamWithCurl(url, &stream);
 		return stream.str();
 	});
-	return handle;
 }
